2a2976bf512946e6a19ab718029cdcbafe1de60f
Anthony <anthonyoutub@gmail.com>
2023-04-10 19:21:52 +0200

    Fix bouton to add card and navbar

--- LearnIO/CreeUIView.swift
+++ LearnIO/CreeUIView.swift
@@ -1,58 +1,52 @@
 import SwiftUI
 
 struct CreeUIView: View {
-    
     @State private var titleInput = ""
     @State private var subtitleInput = ""
     @Binding var listes: [List]
-
+    
     var body: some View {
-        NavigationView {
-            VStack {
-                
-                TextEditor(text: $titleInput)
-                    .frame(height: 200)
+        VStack {
+            TextEditor(text: $titleInput)
+                .frame(height: 200)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.blue, lineWidth: 2)
+                )
+                .padding()
+            
+            TextEditor(text: $titleInput)
+                .frame(height: 200)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.blue, lineWidth: 2)
+                )
+                .padding()
+            
+            Button(action: {
+                // action à effectuer lors du clic sur le bouton
+            }) {
+                Text("Ajouter la carte")
+                    .fontWeight(.bold)
+                    .font(.system(size: 20))
+                    .foregroundColor(.white)
                     .padding()
                     .background(
                         RoundedRectangle(cornerRadius: 10)
                             .stroke(Color.blue, lineWidth: 2)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color.blue)
+                            )
                     )
-                    .padding()
-                
-                TextEditor(text: $titleInput)
-                    .frame(height: 200)
-                    .padding()
-                    .background(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.blue, lineWidth: 2)
-                    )
-                    .padding()
-                
-                
-                Button(action: {
-                    // action à effectuer lors du clic sur le bouton
-                }) {
-                    Text("Ajouter la carte")
-                        .fontWeight(.bold)
-                        .font(.system(size: 20))
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.blue, lineWidth: 2)
-                                .background(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .fill(Color.blue)
-                                )
-                        )
-                }
-                Spacer()
-
             }
+            Spacer()
         }
+        .navigationBarTitle("Crée")
     }
 }
-
 
 
 
