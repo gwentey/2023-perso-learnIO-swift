d2afb34489c38e44801b974afb9328467fe3faea
Anthony <anthonyoutub@gmail.com>
2023-04-24 14:02:10 +0200

    Add SplashScreenView

--- LearnIO/LearnIOApp.swift
+++ LearnIO/LearnIOApp.swift
@@ -13,8 +13,7 @@ struct LearnIOApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            SplashScreenView()
         }
     }
 }
